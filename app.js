ac334d4c11969e2a84e43385380f26e4590dfe4a
Luke Schuyler <lukeschuyler1@gmail.com>
2017-01-09 15:40:50 -0600

    adding initial files

---
+++ app.js
@@ -0,0 +1,11 @@
+var p = new Promise(function(resolve,reject) {
+	$.ajax({
+		url: 'http://www.omdbapi.com/?'
+	}).done(function(data) {
+		resolve(data)
+	})
+});
+
+p.then(function(val) {
+	console.log(val)
+})
